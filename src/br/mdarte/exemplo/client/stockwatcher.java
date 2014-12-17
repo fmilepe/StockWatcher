b3b2d479de2f642beab7067412323270f02e685e
Felipe Milepe <fmilepe@gmail.com>
2014-12-17 13:52:52 -0200

    Criando ajax para retornar json pra tabela.

--- src/br/mdarte/exemplo/client/stockwatcher.java
+++ src/br/mdarte/exemplo/client/stockwatcher.java
@@ -60,9 +60,18 @@ public class stockwatcher implements EntryPoint {
 		stocksFlexTable.setText(0, 2, "Change");  
 		stocksFlexTable.setText(0, 3, "Remove");
 
+		// Add styles to elements in the stock list table.
+		stocksFlexTable.getRowFormatter().addStyleName(0, "watchListHeader");
+		stocksFlexTable.addStyleName("watchList");
+
+		stocksFlexTable.getCellFormatter().addStyleName(0, 1, "watchListNumericColumn");
+		stocksFlexTable.getCellFormatter().addStyleName(0, 2, "watchListNumericColumn");
+		stocksFlexTable.getCellFormatter().addStyleName(0, 3, "watchListRemoveColumn");
+
 		// Assemble Add Stock panel.
 		addPanel.add(newSymbolTextBox);
 		addPanel.add(addStockButton);
+		addPanel.addStyleName("addPanel");
 
 		// Assemble Main panel.
 		mainPanel.add(stocksFlexTable);
@@ -139,8 +148,19 @@ public class stockwatcher implements EntryPoint {
 
 		// Populate the Price and Change fields with new data.
 		stocksFlexTable.setText(row, 1, priceText);
-		stocksFlexTable.setText(row, 2, changeText + " (" + changePercentText
-				+ "%)");
+		Label changeWidget = (Label)stocksFlexTable.getWidget(row, 2);
+		changeWidget.setText(changeText + " (" + changePercentText + "%)");
+		
+		// Change the color of text in the Change field based on its value.
+	    String changeStyleName = "noChange";
+	    if (price.getChangePercent() < -0.1f) {
+	      changeStyleName = "negativeChange";
+	    }
+	    else if (price.getChangePercent() > 0.1f) {
+	      changeStyleName = "positiveChange";
+	    }
+
+	    changeWidget.setStyleName(changeStyleName);
 	}
 
 	/**
@@ -181,17 +201,26 @@ public class stockwatcher implements EntryPoint {
 
 		// Add the stock to the table.
 		int row = stocksFlexTable.getRowCount();
+
 		stocks.add(symbol);
+
 		stocksFlexTable.setText(row, 0, symbol);
+		stocksFlexTable.setWidget(row, 2, new Label());
+		stocksFlexTable.getCellFormatter().addStyleName(row, 1, "watchListNumericColumn");
+		stocksFlexTable.getCellFormatter().addStyleName(row, 2, "watchListNumericColumn");
+		stocksFlexTable.getCellFormatter().addStyleName(row, 3, "watchListRemoveColumn");
 
 		// Add a button to remove this stock from the table.
 		Button removeStockButton = new Button("x");
+		removeStockButton.addStyleDependentName("remove");
 		removeStockButton.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
 				int removedIndex = stocks.indexOf(symbol);
-				stocks.remove(removedIndex);        stocksFlexTable.removeRow(removedIndex + 1);
+				stocks.remove(removedIndex);        
+				stocksFlexTable.removeRow(removedIndex + 1);
 			}
 		});
+
 		stocksFlexTable.setWidget(row, 3, removeStockButton);
 
 		// Get the stock price.
